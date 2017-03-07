df288acdccec19f00b11907f8b99c4da75d82fa8
DreamLCM <CM@CM.local>
2017-03-07 17:27:19 +0800

    Update

--- CHTextFieldViewDemo/CHTextFieldViewDemo/CHTextFieldView/CMView.swift
+++ CHTextFieldViewDemo/CHTextFieldViewDemo/CHTextFieldView/CMView.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class CMView: UIView {
 
-    typealias DoWhat = () -> Void
-    var doWhat: DoWhat?
+    typealias ButtonDoWhat = () -> Void
+//    var buttonDoWhat: ButtonDoWhat?
     
     typealias DoWhatForBeginEditing = () -> Void
-    var doWhatForBeginEditing: DoWhatForBeginEditing?
+//    var doWhatForBeginEditing: DoWhatForBeginEditing?
     
     typealias DoWhatForShouldReturn = () -> Void
     var doWhatForShouldReturn: DoWhatForShouldReturn?
@@ -24,9 +24,10 @@ class CMView: UIView {
         super.init(frame: frame)
                 
     }
-    
 
-    convenience init(frame: CGRect, labelTitle: String, labelColor: UIColor, labelSize: CGFloat) {
+
+    
+    convenience init(frame: CGRect, labelTitle: String, labelColor: UIColor, labelSize: CGFloat,lineColor: UIColor,lineHeight: CGFloat,buttonDoWhat: @escaping ButtonDoWhat,doWhatForBeginEditing: @escaping DoWhatForBeginEditing) {
         self.init(frame: frame)
         
         let label = CMLabel(frame: CGRect(x: 12, y: 8, width: 200, height: 20), title: labelTitle, color: labelColor, sizeTitle: labelSize)
@@ -36,21 +37,21 @@ class CMView: UIView {
         self.addSubview(tf)
         
         tf.doWhatForBeginEditing = {
-            self.doWhatForBeginEditing?()
+            doWhatForBeginEditing()
         }
         
         tf.doWhatForShouldReturn = {
             self.doWhatForShouldReturn?()
         }
         
-        let frame = CGRect(x: 12, y: 8+8+8+label.frame.height+tf.frame.height, width: self.frame.size.width - 24, height: 1)
-        let viewLine = CMLineView(frame: frame, backgroundColor: UIColor.blue)
+        let frame = CGRect(x: 12, y: 8+8+8+label.frame.height+tf.frame.height, width: self.frame.size.width - 24, height: lineHeight)
+        let viewLine = CMLineView(frame: frame, backgroundColor: lineColor)
         self.addSubview(viewLine)
         
         
         
         let button = CMButton(image: #imageLiteral(resourceName: "logo_black_sm")) {
-            self.doWhat?()
+            buttonDoWhat()
         }
         
         button.frame = CGRect(x: self.frame.width - 13 - 22, y: self.frame.height/2-13, width: 26, height: 26)
--- CHTextFieldViewDemo/CHTextFieldViewDemo/ViewController.swift
+++ CHTextFieldViewDemo/CHTextFieldViewDemo/ViewController.swift
@@ -13,17 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let cmView = CMView(frame: CGRect(x: 0, y: 80, width: self.view.frame.size.width, height: 74), labelTitle: "Avalible (BTC)", labelColor: UIColor.black, labelSize: 14)
-        self.view.addSubview(cmView)
+        let frame = CGRect(x: 0, y: 80, width: self.view.frame.size.width, height: 74)
         
-        cmView.doWhat = {
-            print("惦记")
-        }
-        
-        cmView.doWhatForBeginEditing = {
+        let cmView = CMView(frame: frame, labelTitle: "Avalible (BTC)", labelColor: UIColor.black, labelSize: 14, lineColor: UIColor.gray, lineHeight: 1, buttonDoWhat: {
+            
+            print("点击按钮")
+            
+        }) {
             print("编辑键盘")
+            
         }
-
+        
+        self.view.addSubview(cmView)
     }
 
     override func didReceiveMemoryWarning() {
